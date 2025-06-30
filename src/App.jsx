dba4348685d3f0c24742c408e005d4462b42639e
Sara <sarailic431@gmail.com>
2025-06-30 15:06:39 +0200

    setting timer

--- src/App.jsx
+++ src/App.jsx
@@ -9,51 +9,50 @@ function App() {
     movies: [{name: "The Godfather", selected: false, }, {name: "Titanic", selected: false,},{name: "Inception",selected: false,}, 
              {name: "Gladiator", selected: false,}, {name: "Casablanca", selected: false,}, {name: "Psycho", selected: false,},
              {name: "Avatar", selected: false,}, {name: "Jaws", selected: false,}, {name: "Frozen", selected: false,},
-             {name: "The Matrix", selected: false, }, {name: "Goodfellas", selected: false,}, {name: "Braveheart", selected: false,},
-             {name: "Fight Club", selected: false,}, {name: "Pulp Fiction", selected: false,}, {name: "Forrest Gump", selected: false,}, 
-             {name: "The Lion King", selected: false,}, {name: "Back to the Future", selected: false,},  
-             {name: "Blade Runner", selected: false,}, {name: "Star Wars", selected: false,}, {name: "The Dark Knight", selected: false,}, 
-             {name: "The Big Lebowski", selected: false,}, {name: "The Wizard of Oz", selected: false,},  
+             {name: "The Matrix", selected: false, }, {name: "Braveheart", selected: false,},
+             {name: "Fight Club", selected: false,},
+             {name: "The Lion King", selected: false,},
+             {name: "Blade Runner", selected: false,}, {name: "Star Wars", selected: false,}, 
+             {name: "The Big Lebowski", selected: false,}, 
              {name: "Toy Story", selected: false,}, {name: "Alien", selected: false,}, 
              {name: "Interstellar", selected: false,}, {name: "La La Land", selected: false,}, 
             ],
 
     // prettier-ignore
     tvShows: [{name: "Breaking Bad", selected: false,}, {name: "South Park", selected: false,}, {name: "Game of Thrones",selected: false,}, 
-              {name: "Stranger Things", selected: false,}, {name: "Bridgertons", selected: false,}, {name: "The Sopranos", selected: false,}, 
+              {name: "Bridgertons", selected: false,}, {name: "The Sopranos", selected: false,}, 
               {name: "Friends", selected: false,}, {name: "The Office", selected: false,}, {name: "Sherlock", selected: false,}, 
               {name: "Black Mirror", selected: false,}, {name: "The Crown", selected: false,}, {name: "Westworld", selected: false,}, 
               {name: "Better Call Saul", selected: false,}, {name: "The Simpsons", selected: false,}, 
-              {name: "Arrested Development", selected: false,}, {name: "Succession", selected: false,}, {name: "Fargo", selected: false,}, 
-              {name: "Mad Men", selected: false,}, {name: "The West Wing", selected: false,}, {name: "Dexter", selected: false,}, 
+              {name: "Arrested Development", selected: false,}, {name: "Succession", selected: false,},
+              {name: "Mad Men", selected: false,}, {name: "Dexter", selected: false,}, 
               {name: "Rick and Morty", selected: false,}, {name: "Lost", selected: false,}, {name: "Doctor Who", selected: false,}, 
               {name: "The Walking Dead", selected: false,}, {name: "Peaky Blinders", selected: false,}, 
               {name: "Buffy the Vampire Slayer", selected: false,}, {name: "The Big Bang Theory", selected: false,}, 
-              {name: "Ted Lasso", selected: false,}, {name: "True Detective", selected: false,}, 
+              {name: "True Detective", selected: false,}, 
              ],
 
     // prettier-ignore
     countries: [{name: "Australia", selected: false,}, {name: "Brazil", selected: false,}, {name: "Canada", selected: false,}, 
-                {name: "Denmark", selected: false,}, {name: "Egypt", selected: false,}, {name: "France", selected: false,}, 
+                {name: "Egypt", selected: false,}, {name: "France", selected: false,}, 
                 {name: "Germany", selected: false,}, {name: "Hungary", selected: false,}, {name: "India", selected: false,}, 
-                {name: "Japan", selected: false,}, {name: "Kenya", selected: false,}, {name: "Luxembourg", selected: false,}, 
-                {name: "Mexico", selected: false,}, {name: "Netherlands", selected: false,}, {name: "Oman", selected: false,}, 
+                {name: "Japan", selected: false,}, {name: "Kenya", selected: false,},
+                {name: "Mexico", selected: false,}, 
                 {name: "Peru", selected: false,}, {name: "Qatar", selected: false,}, {name: "Russia", selected: false,}, 
                 {name: "Spain", selected: false,}, {name: "Thailand", selected: false,}, {name: "United Kingdom", selected: false,}, 
-                {name: "Vietnam", selected: false,}, {name: "Italy", selected: false,}, {name: "United States", selected: false,}, 
+                {name: "Italy", selected: false,},
                 {name: "China", selected: false,}, {name: "South Africa", selected: false,}, {name: "New Zealand", selected: false,}, 
-                {name: "Argentina", selected: false,}, {name: "Belgium", selected: false,}, {name: "Chile", selected: false,}, 
+                {name: "Argentina", selected: false,}, {name: "Chile", selected: false,}, 
               ],
 
     // prettier-ignore
     animals: [{name: "Elephant", selected: false,}, {name: "Lion", selected: false,}, {name: "Penguin", selected: false,}, 
-              {name: "Dolphin", selected: false,}, {name: "Tiger", selected: false,}, {name: "Kangaroo", selected: false,}, {name: "Panda", selected: false,}, 
+              {name: "Dolphin", selected: false,}, {name: "Tiger", selected: false,}, {name: "Panda", selected: false,}, 
               {name: "Zebra", selected: false,}, {name: "Polar Bear", selected: false,}, {name: "Cheetah", selected: false,}, 
               {name: "Buffalo", selected: false,}, {name: "Koala", selected: false,}, {name: "Gorilla", selected: false,}, 
-              {name: "Crocodile", selected: false,}, {name: "Flamingo", selected: false,}, {name: "Peacock", selected: false,}, {name: "Jaguar", selected: false,}, 
-              {name: "Leopard", selected: false,}, {name: "Wolf", selected: false,}, {name: "Fox", selected: false,}, {name: "Bald Eagle", selected: false,}, 
-              {name: "Owl", selected: false,}, {name: "Frog", selected: false,}, {name: "Octopus", selected: false,}, 
-              {name: "Turtle", selected: false,}, {name: "Snake", selected: false,}, 
+              {name: "Flamingo", selected: false,}, {name: "Peacock", selected: false,}, {name: "Jaguar", selected: false,}, 
+              {name: "Leopard", selected: false,}, {name: "Wolf", selected: false,}, {name: "Bald Eagle", selected: false,}, 
+              {name: "Owl", selected: false,}, {name: "Frog", selected: false,}, {name: "Octopus", selected: false,},  
              ],
   };
 
@@ -63,6 +62,8 @@ function App() {
   const alfabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("").map((letter) => {
     return { letter, isClicked: false };
   });
+
+  const [time, setTime] = useState(0);
 
   const [information, setInformation] = useState(info);
 
@@ -130,6 +131,26 @@ function App() {
       ].name.toUpperCase();
   }
 
+  const timerFunction = function () {
+    let time = 14;
+
+    const tick = function () {
+      time--;
+
+      let timeString = String(time);
+      setTime(timeString);
+
+      console.log(time);
+
+      if (time === 0) {
+        clearInterval(timer);
+      }
+    };
+
+    tick();
+    const timer = setInterval(tick, 1000);
+  };
+
   return (
     <>
       <nav>
@@ -147,13 +168,13 @@ function App() {
       </nav>
 
       {!play && (
-        <div className="overlay">
+        <div className="overlay" onClick={() => timerFunction()}>
           <h2>Chose Mode:</h2>
 
           <div className="mode-container">
             <button
-              className={`btn category-btn ${
-                mode === "easy" ? "active-category" : ""
+              className={`btn mode-btn ${
+                mode === "easy" ? "active-mode" : ""
               } `}
               onClick={() => {
                 setMode("easy");
@@ -162,9 +183,7 @@ function App() {
               Easy
             </button>
             <button
-              className={`btn category-btn ${
-                mode !== "easy" ? "active-category" : ""
-              }`}
+              className={`btn mode-btn ${mode !== "easy" ? "active-mode" : ""}`}
               onClick={() => setMode("difficult")}
             >
               Difficult
